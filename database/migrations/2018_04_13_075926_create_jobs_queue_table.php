1ba1bdeadbd993649fb8e2ad561be0c1afb40f80
kosarglowlogix <kosar@glowlogix.com>
2018-09-28 00:09:31 +0500

    jobs_ques table added

--- database/migrations/2018_04_13_075926_create_jobs_queue_table.php
+++ database/migrations/2018_04_13_075926_create_jobs_queue_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobsQueueTable extends Migration
+class CreateJobsQueuesTable extends Migration
 {
     /**
      * Run the migrations.
