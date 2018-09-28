c97e66121f1813a2ceeb75f082a94e80c1fdcdf9
kosarglowlogix <kosar@glowlogix.com>
2018-09-28 22:26:08 +0500

    redirect to home on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/dashboard';
+    protected $redirectTo = '/admin/home';
 
     /**
      * Create a new controller instance.
