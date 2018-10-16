117f2a6fb4e9e8d44d9792126ba3cb95a1bbf71d
awaid <awaid@glowlogix.com>
2018-10-16 21:31:16 +0500

    added design files.

---
+++ resources/views/admin/employees/organizationalStructure.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('Heading')
+<h3 class="text-themecolor">Employees</h3>
+<ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+    <li class="breadcrumb-item active">People Management</li>
+    <li class="breadcrumb-item active">Employees</li>
+</ol>
+@stop
+@section('content')
+@stop
