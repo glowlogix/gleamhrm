7d658a33f1b6121742bafc9ee72c39b0101a5c77
awaid <awaid@glowlogix.com>
2018-11-01 23:34:33 +0500

    Bugs Phase II

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends Controller
        $this->meta['title'] = 'Applicants';
        $applicants = Applicant::where('recruited', 0)->take(10)->get();
         
-        return view('admin.dashboard.index',$this->metaResponse())->with('employee',Employee::take(5)->get()->sortByDesc("id"));
+        return view('admin.dashboard.index',$this->metaResponse())->with('employee',Employee::take(5)->get()->sortByDesc("id"))->with('employees',Employee::where('employment_status','permanent')->orwhere('employment_status','probation')->get());
     }
 //    Help
     public function help()
