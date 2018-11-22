c40676acdacaf45e85cf3f3f293f6252171b3d36
awaid <awaid@glowlogix.com>
2018-11-22 23:03:16 +0500

    Vendors,Vendor Categories,Departments

--- app/Employee.php
+++ app/Employee.php
@@ -60,7 +60,7 @@ class Employee extends Authenticatable
         }*/
     }
     public function department(){
-        return $this->belongsTo('App\department', 'department_id');
+        return $this->belongsTo('App\Department', 'department_id');
     }
 
 }
