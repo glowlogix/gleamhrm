270adf0662be21f3ad1a269ee402c2182403baa4
Johar Alam <johar@glowlogix.com>
2021-08-05 16:59:21 +0500

    System updates for framework and AdminLTE theme

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.PNG')}}">
+    <title>HR | Glowlogix</title>
+
+    <!-- Google Font: Source Sans Pro StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/font-googleapis/font.css') }}">
+
+    <!-- Font Awesome Icons StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/fontawesome-free/css/all.min.css') }}">
+
+    <!-- Themify Icons StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/themify-icons/themify-icons.css') }}">
+
+    <!-- Material Design Icons StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/material-icons/css/materialdesignicons.min.css') }}">
+
+    <!-- IonIcons StyleSheet -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <!-- Theme  StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/dist/css/adminlte.min.css') }}">
+
+    <!-- Overlay Scrollbar StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+
+    <!-- DataTables StyleSheet -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/backend/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+    <!-- jQuery Script -->
+    <script src="{{ asset('assets/backend/plugins/jquery/jquery.min.js') }}"></script>
+
+    <!-- Chart Script -->
+    <script src="{{ asset('assets/backend/plugins/Chart.js/Chart.min.js') }}"></script>
+    
+    <!-- jquery-validation -->
+    <script src="{{ asset('assets/backend/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+  </head>
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+      <!-- Navbar Start -->
+      @include('layouts.partials.navbar')
+      <!-- Navbar End -->
+
+      <!-- Left Sidebar Container Start -->
+      @include('layouts.partials.left-sidebar')
+      <!-- Left Sidebar Container End -->
+
+      <!-- Page Content Start -->
+      <div class="content-wrapper">
+        @yield('content')
+      </div>
+      <!-- Page Content End -->
+
+      <!-- Right Sidebar Container Start -->
+      @include('layouts.partials.right-sidebar')
+      <!-- Right Sidebar Container End -->
+
+      <!-- Main Footer Start -->
+      @include('layouts.partials.footer')
+      <!-- Main Footer End -->
+    </div>
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- Bootstrap Script -->
+    <script src="{{ asset('assets/backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- AdminLTE Script -->
+    <script src="{{ asset('assets/backend/dist/js/adminlte.js') }}"></script>
+
+    <!-- Overlay Scrollbar Script -->
+    <script src="{{ asset('assets/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <!-- DataTables  & Plugins Script -->
+    <script src="{{ asset('assets/backend/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+  </body>
+</html>
---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,172 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <!-- <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li> -->
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+
+      <!-- Messages Dropdown Menu -->
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start
+            <div class="media">
+              <img src="{{ asset('assets/backend/dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start
+            <div class="media">
+              <img src="{{ asset('assets/backend/dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start
+            <div class="media">
+              <img src="{{ asset('assets/backend/dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li> -->
+      <!-- Notifications Dropdown Menu -->
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-bell"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{asset(Auth::user()->picture)}}" onerror="this.src ='{{asset('assets/images/default.png')}}';" alt="user" class="brand-image img-circle elevation-3" height="30px" width="30px"/></a>
+        <div class="dropdown-menu dropdown-menu-right scale-up mr-2">
+          <ul class="dropdown-user pl-3 pr-3">
+            <div class="row">
+              <div class="col-5 d-none d-lg-block d-md-block">
+                <img  src="{{asset(Auth::user()->picture)}}" onerror="this.src ='{{asset('assets/images/default.png')}}';" alt="user" class="brand-image img-circle elevation-3" width="80px">
+              </div>
+              <div class="col-sm-12 d-lg-none d-md-none d-sm-block">
+                <img  src="{{asset(Auth::user()->picture)}}" onerror="this.src ='{{asset('assets/images/default.png')}}';" alt="user" class="brand-image img-circle elevation-3" width="50px">
+              </div>
+              <div class="col-lg-7 col-md-7 col-sm-12">
+                <h4 class="mb-0">{{Auth::user()->firstname}}</h4>
+                <div>
+                  <p class="text-muted mb-1" style="font-size:12px;">{{Auth::user()->official_email}}</p>
+                </div>
+                <div>
+                  <a href="{{route('profile.index')}}" class="btn btn-rounded btn-danger btn-sm">View Profile</a>
+                </div>
+              </div>
+            </div>
+            <hr class="mt-4 mb-2">
+            <a href="{{route('profile.index')}}" class="text-muted">
+              <div>
+                <i class="fas fa-cog"></i> Account Setting
+              </div>
+            </a>
+            <hr class="mt-2 mb-2">
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('navbar-logout').submit();" class="link text-muted" data-toggle="tooltip" title="Logout">
+              <div>
+                <i class="fa fa-power-off"></i> Logout
+              </div>
+            </a>
+          </ul>
+          <form id="navbar-logout" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+        </div>
+      </li>
+    </ul>
+</nav>
