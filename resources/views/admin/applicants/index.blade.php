bb53348a507b4b0b94b130285de0f73272145bd2
Johar Alam <johar@glowlogix.com>
2021-08-20 17:50:35 +0500

    Implementation of applications CRUD with AdminLTE v3.0

--- resources/views/admin/applicants/index.blade.php
+++ resources/views/admin/applicants/index.blade.php
@@ -1,57 +1,92 @@
-@extends('layouts.admin')
-@section('Heading')
-	<button type="button" class="btn btn-info btn-rounded m-t-10 float-right" onclick="window.location.href='{{route('applicants.hired')}}'"> Hired</button>
-	<h3 class="text-themecolor">Applicants</h3>
-	<ol class="breadcrumb">
-	<li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-	<li class="breadcrumb-item active">Applicants</li>
-	</ol>
-@stop
+@extends('layouts.master')
+
 @section('content')
-	{{--//////startt--}}
-		<div class="col-12">
-		<div class="card">
-			<div class="card-body">
-				<div class="table-responsive">
-					<table class="table">
-						@if($applicants->count() > 0)
-						<thead>
-						<tr>
-							<th>Avatar</th>
-							<th>Name</th>
-							<th>City</th>
-							<th>Job Status</th>
-							<th>Applied For</th>
-							<th>CV</th>
-							<th>Actions</th>
-						</tr>
-						</thead>
-						<tbody>
-						@foreach($applicants as $applicant)
-						<tr>
-							<td><img src="{{asset($applicant->avatar)}}" alt="user" width="40" class="img-circle" /></td>
-							<td>
-								<a>{{$applicant->name}}</a>
-							</td>
-							<td>{{$applicant->city}}</td>
-							<td>{{$applicant->job_status}}</td>
-							<td>{{$applicant->job->title}}</td>
-							<td><a target="_blank" href="{{asset($applicant->cv)}}" class="fas fa-file-alt text-inverse font-bold" data-toggle="tooltip"  data-original-title="Click To Open CV" style="font-size: 30px">
-								</a>
-							</td>
-							<td class="text-nowrap">
-								<a href="{{route('applicant.hire',['id'=>$applicant->id])}}" data-toggle="tooltip"  data-original-title="Hire" class="btn btn-sm btn-info"> <i class="fas fa-pencil-alt "></i></a>
-								<a href="{{route ('applicant.delete',['id' =>$applicant->id])}}" data-toggle="tooltip"   data-original-title="Delete" class="btn btn-sm btn-danger"> <i class="fas fa-window-close"></i></a>
-							</td>
-						</tr>
-						@endforeach
-						@else
-							<p class="text-center" >No New Applicant Found</p>
-						@endif
-						</tbody>
-					</table>
+<!-- Breadcrumbs Start -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Applicants</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ url('applicant') }}">Hiring</a></li>
+          <li class="breadcrumb-item active">Application</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumbs End -->
+
+<!-- Main Content Start -->
+<div class="content">
+  <div class="container-fluid">
+  	<div class="row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body">
+						<div class="text-right">
+							<button type="button" class="btn btn-info btn-rounded" onclick="window.location.href='{{route('applicants.hired')}}'" title="Show Hired Applicants"><i class="mdi mdi-briefcase-check"></i><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Hired Applicants</span></button>
+						</div>
+			      		
+			      <hr>
+
+						<div class="table-responsive">
+							<table id="applicants" class="table table-bordered table-striped table-hover">
+								<thead>
+									<tr>
+										<th>Avatar</th>
+										<th>Name</th>
+										<th>City</th>
+										<th>Job Status</th>
+										<th>Applied For</th>
+										<th>CV</th>
+										<th>Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($applicants as $applicant)
+										<tr>
+											<td><img src="{{ asset($applicant->avatar) }}" alt="user-image" width="40" class="img-circle" /></td>
+											<td>
+												<a>{{$applicant->name}}</a>
+											</td>
+											<td>{{$applicant->city}}</td>
+											<td>{{$applicant->job_status}}</td>
+											<td>{{$applicant->job->title}}</td>
+											<td>
+												<a target="_blank" href="{{asset($applicant->cv)}}" class="fas fa-file-alt text-inverse font-bold" data-toggle="tooltip"  data-original-title="Click To Open CV" style="font-size: 30px">
+												</a>
+											</td>
+											<td class="text-nowrap">
+												<a href="{{route('applicant.hire',['id'=>$applicant->id])}}" data-toggle="tooltip" title="Hire Applicant" class="btn btn-sm btn-success"> <i class="mdi mdi-briefcase-check"></i></a>
+												<a href="{{route ('applicant.delete',['id' =>$applicant->id])}}" data-toggle="tooltip" title="Delete Applicant" class="btn btn-sm btn-danger"> <i class="fas fa-trash-alt"></i></a>
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<!-- Main Content End -->
+
+<script>
+  $(document).ready(function () {
+    $('#applicants').DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 @stop
