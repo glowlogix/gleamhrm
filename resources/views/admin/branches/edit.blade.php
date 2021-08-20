15ea73d62c9158b0283ecedb70123e1bed9f0d0b
Johar Alam <johar@glowlogix.com>
2021-08-20 19:22:26 +0500

    Implementation of branches CRUD with AdminLTE v3.0

--- resources/views/admin/branches/edit.blade.php
+++ resources/views/admin/branches/edit.blade.php
@@ -1,106 +1,193 @@
-@extends('layouts.admin')
-@section('Heading')
-	<h3 class="text-themecolor">Edit Branch</h3>
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-		<li class="breadcrumb-item active">Settings</li>
-		<li class="breadcrumb-item active">Branch</li>
-		<li class="breadcrumb-item active">Edit</li>
-	</ol>
-@stop
+@extends('layouts.master')
+
 @section('content')
-<div class="row">
-	<div class="col-lg-12">
-		<div class="card card-outline-info">
-			<div class="card-body">
-				<form  action="{{route('branch.update',['id'=>$office_location->id])}}" method="post" enctype="multipart/form-data">
-					<input name="_method" type="hidden" value="PUT">
-					{{csrf_field()}}
-					<div class="form-body">
-						<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Name</label>
-								<input  type="text" name="name" placeholder="Enter name here" class="form-control"  value="{{old('name', $office_location->name)}}">
-							</div>
-						</div>
-							<div class="col-md-6">
-								<div class="form-group has-success">
-									<label class="control-label">Status</label>
-									{{ csrf_field() }}
-									<select name="status" id="status" class="form-control custom-select" >
-										<option value="1"@if($office_location->status == 1) selected @endif>Active</option>
-										<option value="2"@if($office_location->status == 2) selected @endif>Inactive</option>
-									</select>
+<!-- Breadcrumbs Start -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Edit Branch</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ url('branch') }}">Settings</a></li>
+          <li class="breadcrumb-item"><a href="{{ url('branch') }}">Branches</a></li>
+          <li class="breadcrumb-item active">Edit</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumbs End -->
+
+<!-- Error Message Section Start -->
+@if ($errors->any())
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-danger">
+                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                        @foreach ($errors->all() as $error)
+                          <li><strong>Error!</strong> {{ $error }}</li>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+
+@if(Session::has('error'))
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-danger" align="left">
+                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                        <strong>Error!</strong> {{Session::get('error')}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+@if(Session::has('success'))
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-success" align="left">
+                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                        <strong>Success!</strong> {{Session::get('success')}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+<!-- Error Message Section End -->
+
+<!-- Main Content Start -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+						<form id="editBranchForm" action="{{route('branch.update',[$office_location->id])}}" method="post" enctype="multipart/form-data">
+							<input name="_method" type="hidden" value="PUT">
+							{{csrf_field()}}
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<label class="control-label">Name</label>
+										<input  type="text" name="name" placeholder="Enter name here" class="form-control"  value="{{old('name', $office_location->name)}}">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group has-success">
+										<label class="control-label">Status</label>
+										{{ csrf_field() }}
+										<select name="status" id="status" class="form-control custom-select" >
+											<option value="">Select Status</option>
+											<option value="1"@if($office_location->status == 1) selected @endif>Active</option>
+											<option value="2"@if($office_location->status == 2) selected @endif>Inactive</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label class="control-label">Timing Start</label>
+										<input class="form-control " type="time" name="timing_start" placeholder="Enter Timing Start here"   id="timing_start" value="{{old('timing_start', Carbon\Carbon::parse($office_location->timing_start)->format('h:i'))}}">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label class="control-label">Timing OFF</label>
+										<input class="form-control" type="time" name="timing_off" placeholder="Enter Timing Off here"  value="{{old('timing_off', Carbon\Carbon::parse($office_location->timing_off)->format('h:i'))}}" />
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label class="control-label">Address</label>
+										<input type="text" name="address" class="form-control" placeholder="Enter Address here" value="{{old('address', $office_location->address)}}">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label class="control-label">Phone#</label>
+										<input type="number" name="phone_number" class="form-control" placeholder="Enter Phone Number here" value="{{old('phone_number', $office_location->phone_number)}}">
+									</div>
+								</div>	
+								<div class="col-md-12">
+									<div class="form-group">
+										<label class="control-label">Select Weekend</label>
+										<select multiple class="form-control custom-select" data-placeholder="Choose Skills" tabindex="1" name="weekend[]">
+											@foreach($weekDays as $weekDay)
+												<option value="{{$weekDay}}" @if($office_location->weekend!=null)@foreach(json_decode($office_location->weekend) as $key) @if($weekDay == $key) selected @endif @endforeach @endif>{{$weekDay}}</option>
+											@endforeach
+										</select>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Timing Start</label>
-								<input class="form-control " type="time" name="timing_start" placeholder="Enter Timing Start here"   id="timing_start" value="{{old('timing_start', Carbon\Carbon::parse($office_location->timing_start)->format('h:i'))}}">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Timing OFF</label>
-								<input class="form-control" type="time" name="timing_off" placeholder="Enter Timing Off here"  value="{{old('timing_off', Carbon\Carbon::parse($office_location->timing_off)->format('h:i'))}}" />
-							</div>
-						</div>
-						</div>
-						<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Address</label>
-								<input type="text" name="address" class="form-control" placeholder="Enter Address here" value="{{old('address', $office_location->address)}}">
-						</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Phone#</label>
-								<input type="number" name="phone_number" class="form-control" placeholder="Enter Phone Number here" value="{{old('phone_number', $office_location->phone_number)}}">
-							</div>
-						</div>	<div class="col-md-6">
-							<div class="form-group">
-								<label class="control-label">Select Weekend</label>
-								<select multiple class="form-control custom-select" data-placeholder="Choose Skills" tabindex="1" name="weekend[]">
-									@foreach($weekDays as $weekDay)
-										<option value="{{$weekDay}}" @if($office_location->weekend!=null)@foreach(json_decode($office_location->weekend) as $key) @if($weekDay == $key) selected @endif
-												@endforeach @endif>{{$weekDay}}</option>
-									@endforeach
-								</select>
-							</div>
-						</div>
+
+							<hr>
+
+							<button type="submit" class="btn btn-primary"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-check-circle" title="Update Branch"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Update</span></button>
+							<button type="button" onclick="window.location.href='{{route('branch.index')}}'" class="btn btn-default" title="Cancel"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-window-close"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Cancel</span></button>
+						</form>
 					</div>
-					<div class="form-actions">
-						&nbsp;&nbsp;&nbsp;<button type="submit" class="btn btn-success">Update Location</button>
-						<button type="button" onclick="window.location.href='{{route('branch.index')}}'" class="btn btn-inverse">Cancel</button>
-					</div>
-					</div>
-				</form>
-			</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-@push('scripts')
-<script type="text/javascript">
-$(document).ready(function () {
-    $(function () {
-		$("div.timepicker").on("click", function () {
-	        $(this).datetimepicker({
-	            format: "LT",
-	            icons: {
-	                up: "fa fa-chevron-up",
-	                down: "fa fa-chevron-down"
-	            }
-	        });
-	        console.log("O.K.");
-		});
+<!-- Main Content End -->
 
-    });
-});
+<script type="text/javascript">
+$(function () {
+	  	$('#editBranchForm').validate({
+		    rules: {
+		    	name: {
+		        	required: true
+		     	},
+		     	status: {
+		        	required: true
+		     	},
+				timing_start: {
+		        	required: true
+		     	},
+				timing_off: {
+		        	required: true
+		     	},
+				address: {
+		        	required: true
+		     	},
+				phone_number: {
+		        	required: true
+		     	}
+		    },
+		    messages: {
+		    	name: "Name is required",
+		    	status: "Status is required",
+				timing_start: "Start time is required",
+				timing_off: "Off time is required",
+				address: "Address is required",
+				phone_number: "Phone number is required"
+		    },
+		    errorElement: 'span',
+		    errorPlacement: function (error, element) {
+		      	error.addClass('invalid-feedback');
+		      	element.closest('.form-group').append(error);
+		    },
+		    highlight: function (element, errorClass, validClass) {
+		      	$(element).addClass('is-invalid');
+		    },
+		    unhighlight: function (element, errorClass, validClass) {
+		      	$(element).removeClass('is-invalid');
+		    }
+	  	});
+	});
 </script>
-@endpush
 @stop
