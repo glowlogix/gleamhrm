465d019c54516024ef2004c16dc7e259d4a5faea
Johar Alam <johar@glowlogix.com>
2021-08-20 18:01:19 +0500

    Implementation of roles and permissions CRUD with AdminLTE v3.0

--- resources/views/admin/roles_permissions/create.blade.php
+++ resources/views/admin/roles_permissions/create.blade.php
@@ -1,77 +1,127 @@
-@extends('layouts.admin')
-@section('Heading')
-	<h3 class="text-themecolor">Create Role</h3>
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-		<li class="breadcrumb-item active">Roles Permission</li>
-		<li class="breadcrumb-item active">Create</li>
-	</ol>
-@stop
-@section('content')
+@extends('layouts.master')
 
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card card-outline-info">
-				<div style="margin-top:10px; margin-right: 10px;">
-					<a class="btn btn-info float-right" href="{{route('roles_permissions')}}"> Back</a>
-				</div>
-				<div class="card-body">
-					<form  action="{{route('roles_permissions.store')}}" method="post" enctype="multipart/form-data">
-						{{csrf_field()}}
-						<div class="form-body">
-							<div class="col-md-6">
-								<div class="form-group">
-									<label class="control-label">Name</label>
-									<input  type="text" name="name" placeholder="Enter name here" class="form-control" value="{{old('name')}}">
-									<input type="hidden" name="status" value="1">
+@section('content')
+<!-- Breadcrumbs Start -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Create Role</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ url('rolespermissions') }}">Manage Roles</a></li>
+          <li class="breadcrumb-item"><a href="{{ url('rolespermissions') }}">Roles and Permissions</a></li>
+          <li class="breadcrumb-item active">Create</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumbs End -->
+
+<!-- Session Message Section Start -->
+@include('layouts.partials.session-message')
+<!-- Session Message Section End -->
+
+<!-- Main Content Start -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                    	<div>
+							<a class="btn btn-info" href="{{route('roles_permissions')}}"><i class="fas fa-chevron-left"></i><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Back</span></a>
+						</div>
+
+						<hr>
+						
+						<form id="createRoleForm" action="{{route('roles_permissions.store')}}" method="post" enctype="multipart/form-data">
+							{{csrf_field()}}
+							<div class="row justify-content-between">
+								<div class="col-6">
+									<div class="form-group">
+										<label class="control-label">Name</label>
+										<input  type="text" name="name" placeholder="Enter name here" class="form-control" value="{{old('name')}}">
+										<input type="hidden" name="status" value="1">
+									</div>
+								</div>
+								<div class="col-6 mt-5 text-right">
+									<label for="check_all"><input type="checkbox" class="mr-1" id="check_all"/>Select All</label>
 								</div>
 							</div>
-						<hr>
-							<input type="checkbox" id="check_all"/>
-							<label for="check_all">Select All</label>
-							<br>
-							<br>
+
+							<hr class="mt-0">
+
 							<div class="form-group row">
 								@foreach ($all_controllers as $key => $row)
-									<div class="col-md-4">
-									<hr>
-									<input type="checkbox" class="check_all_sub" id="{{$key}}">
-									<label for="{{$key}}"><b>{{$key}} :</b></label>
-									<br>
-									<div class="{{$key}}">
-										@foreach ($row as $route)
-											<div class="col-md-6">
-											<input type="checkbox" id="{{$key}}:{{$route}}" name="permissions[]" value="web:{{$key}}:{{$route}}">
-											<label for="{{$key}}:{{$route}}">{{$route}}</label>
-											</div>
-										@endforeach
-									</div>
+									<div class="col-md-12">
+										<div class="row">
+											<label class="col-12" for="{{$key}}"><input type="checkbox" class="check_all_sub mr-1" id="{{$key}}"><b>{{$key}}</b></label>
+										</div>
+
+										<hr class="mt-0">
+
+										<div class="{{$key}} row">
+											@foreach ($row as $route)
+												<div class="col-lg-4 col-md-4 col-sm-4">
+													<label class="font-weight-normal col-12" for="{{$key}}:{{$route}}"><input type="checkbox" class="mr-1" id="{{$key}}:{{$route}}" name="permissions[]" value="web:{{$key}}:{{$route}}">{{$route}}</label>
+												</div>
+											@endforeach
+										</div>
+										<br>
 									</div>
 								@endforeach
-						</div>
-						</div>
-						<div class="form-actions">
-							&nbsp;&nbsp;&nbsp;<button type="submit" class="btn btn-success">Create Role</button>
-							<button type="button" onclick="window.location.href='{{route('roles_permissions')}}'" class="btn btn-inverse">Cancel</button>
-						</div>
-					</form>
+							</div>
+
+							<hr>
+
+							<button type="submit" class="btn btn-primary"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-check-circle"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Create Role</span></button>
+							<button type="button" onclick="window.location.href='{{route('roles_permissions')}}'" class="btn btn-default"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-window-close"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Cancel</span></button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<!-- Main Content End -->
 
-@push('scripts')
-<script type="text/javascript">
-$(document).ready(function () {
-    $(function () {
-	    $("#check_all").click(function(){
-		    $('input:checkbox').not(this).prop('checked', this.checked);
-		});
-    	$(".check_all_sub").click(function(){
-		    $('div.'+ this.id +' input:checkbox').prop('checked', this.checked);
-		});
-    });
-});
+<script>
+	$(document).ready(function () {
+	    $(function () {
+		    $("#check_all").click(function(){
+			    $('input:checkbox').not(this).prop('checked', this.checked);
+			});
+	    	$(".check_all_sub").click(function(){
+			    $('div.'+ this.id +' input:checkbox').prop('checked', this.checked);
+			});
+	    });
+	});
+
+	$(function () {
+	  $('#createRoleForm').validate({
+	    rules: {
+	      name: {
+	        required: true,
+	      }
+	    },
+	    messages: {
+	      name: "Role name is required"
+	    },
+	    errorElement: 'span',
+	    errorPlacement: function (error, element) {
+	      error.addClass('invalid-feedback');
+	      element.closest('.form-group').append(error);
+	    },
+	    highlight: function (element, errorClass, validClass) {
+	      $(element).addClass('is-invalid');
+	    },
+	    unhighlight: function (element, errorClass, validClass) {
+	      $(element).removeClass('is-invalid');
+	    }
+	  });
+	});
 </script>
-@endpush
 @stop
--- resources/views/admin/roles_permissions/index.blade.php
+++ resources/views/admin/roles_permissions/index.blade.php
@@ -1,48 +1,73 @@
-@extends('layouts.admin')
-@section('Heading')
-	<button type="button"  onclick="window.location.href='{{route('roles_permissions.create')}}'" class="btn btn-info btn-rounded m-t-10 float-right"><span class="fas fa-plus" ></span> Add Role</button>
-	<h3 class="text-themecolor">Permission And Roles</h3>
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-		<li class="breadcrumb-item active">Roles Permission</li>
-		<li class="breadcrumb-item active">Roles</li>
-	</ol>
-@stop
-@section('content')
-	<div class="row">
-		<div class="col-12">
-			<div class="card">
-				<div class="card-body">
-					<h6 class="card-subtitle"></h6>
-					{{--<button type="button"  onclick="window.location.href='{{route('roles_permissions.applyrole')}}'" class="btn btn-info btn-rounded m-t-10 float-right">Apply Role</button>--}}
-					<br>
-					<div class="table">
-						<table id="demo-foo-addrow" class="table m-t-30 table-hover contact-list" data-paging="true" data-paging-size="7">
-							<thead>
-							<tr>
-								<th>Roles</th>
-								<th>Action</th>
-							</tr>
-							</thead>
-							<tbody>
-							@if($roles->count() > 0)
-								@foreach($roles as $role)
-								<tr>
+@extends('layouts.master')
 
-									<td>{{$role->name}}</td>
-									<td class="text-nowrap">
-										<a class="btn btn-info btn-sm" href="{{route('roles_permissions.edit',['id'=>$role->id])}}" data-toggle="tooltip" data-original-title="Edit"> <i class="fas fa-pencil-alt text-white "></i></a>
-										<a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirm-delete{{ $role->id }}"> <i class="fas fa-window-close text-white"></i> </a>
-										{{--//Model//--}}
+@section('content')
+<!-- Breadcrumbs Start -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Roles and Permissions</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ url('rolespermissions') }}">Manage Roles</a></li>
+          <li class="breadcrumb-item active">Roles and Permissions</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumbs End -->
+
+<!-- Session Message Section Start -->
+@include('layouts.partials.session-message')
+<!-- Session Message Section End -->
+
+<!-- Main Content Start -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+						<div class="text-right">
+							<button type="button" onclick="window.location.href='{{route('roles_permissions.create')}}'" class="btn btn-info btn-rounded"><i class="fas fa-plus"></i><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Add Role</span></button>
+                        </div>
+
+                        <hr>
+                        
+						<div class="table-responsive">
+                            <table id="roles" class="table table-bordered table-striped table-hover">
+								<thead>
+									<tr>
+										<th>Roles</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($roles as $role)
+										<tr>
+											<td>{{$role->name}}</td>
+											<td class="text-nowrap">
+												<a class="btn btn-warning btn-sm" href="{{route('roles_permissions.edit',['id'=>$role->id])}}" data-toggle="tooltip" data-original-title="Edit"> <i class="fas fa-pencil-alt text-white "></i></a>
+												<a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirm-delete{{ $role->id }}"> <i class="fas fa-trash-alt text-white"></i> </a>
+											</td>
+										</tr>
 										<div class="modal fade" id="confirm-delete{{ $role->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 											<div class="modal-dialog">
 												<div class="modal-content">
 													<form action="{{ route('roles_permissions.delete' , $role->id )}}" method="post">
 														{{ csrf_field() }}
 														<div class="modal-header">
-															Are you sure you want to delete this Role {{$role->name}}?
+		                                                    <h4 class="modal-title">Delete Role</h4>
+		                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		                                                        <span aria-hidden="true">×</span>
+		                                                    </button>
+		                                                </div>
+														<div class="modal-body">
+															Are you sure you want to delete Role "{{$role->name}}"?
 														</div>
-														<div class="modal-footer">
+														<div class="modal-footer justify-content-between">
 															<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
 															<button  type="submit" class="btn btn-danger btn-ok">Delete</button>
 														</div>
@@ -50,18 +75,29 @@
 												</div>
 											</div>
 										</div>
-									</td>
-
-								</tr>
-							@endforeach @else
-								<tr> No Permission Found</tr>
-							@endif
-
-							</tbody>
-						</table>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	@stop
+</div>
+<!-- Main Content End -->
+
+<script>
+    $(document).ready(function () {
+        $('#roles').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
+@stop
