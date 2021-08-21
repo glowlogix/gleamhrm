25191ebdd1e1db98701e8e75b5fab95a694c594a
Johar Alam <johar@glowlogix.com>
2021-08-21 11:34:45 +0500

    Implementation of skills CRUD with AdminLTE v3.0

--- resources/views/admin/skills/skill_assign_edit.blade.php
+++ resources/views/admin/skills/skill_assign_edit.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.admin')
+@extends('layouts.master')
 @section('Heading')
     <button type="button" class="btn btn-info btn-rounded m-t-10 float-right" onclick="window.location.href='{{route('skill.index')}}'" >Back</button>
-    <h3 class="text-themecolor">Skill Assigned Employees</h3>
+    <h3 class="text-themecolor"></h3>
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
         <li class="breadcrumb-item active">Setting</li>
@@ -11,59 +11,130 @@
     </ol>
 @stop
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h3 class="card-title">
-                         Employees Having Skill "{{$skill_name->skill_name}}"
-                    </h3>
-                    <div class="table">
-                        <table id="demo-foo-addrow" class="table  m-t-30 table-hover contact-list" data-paging="true" data-paging-size="7">
-                            <thead>
-                            @if($assigned_employees->count() > 0)
-                                <tr>
-                                    <th>#</th>
-                                    <th>Employee_Name</th>
-                                    <th> Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($assigned_employees as $key=>$assigned_employee)
-                                <tr>
-                                    <td>{{$key+1}}</td>
-                                    <td>{{isset($assigned_employee->employee_id) ? $assigned_employee->employee->firstname : ''}}</td>
-                                    <td class="text-nowrap">
-                                        <a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirm-delete{{ $assigned_employee->id }}"  data-original-title="Close"> <i class="fas fa-window-close text-white  "></i> </a>
+<!-- Breadcrumbs Start -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Employees in Skill "{{$skill_name->skill_name}}"</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ url('skills') }}">Settings</a></li>
+          <li class="breadcrumb-item"><a href="{{ url('skills') }}">Skills</a></li>
+          <li class="breadcrumb-item active">Show</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Breadcrumbs End -->
+
+<!-- Error Message Section Start -->
+@if(Session::has('error'))
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-danger" align="left">
+                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                        <strong>Error!</strong> {{Session::get('error')}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+@if(Session::has('success'))
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-success" align="left">
+                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                        <strong>Success!</strong> {{Session::get('success')}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+<!-- Error Message Section End -->
+
+<!-- Main Content Start -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <button type="button" class="btn btn-info" onclick="window.location.href='{{route('skill.index')}}'" title="Back"><i class="fas fa-chevron-left"></i><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Back</span></button>
+
+                        <hr>
+
+                        <div class="table-responsive">
+                            <table id="skilled_employees" class="table table-bordered table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Employee_Name</th>
+                                        <th> Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($assigned_employees as $key=>$assigned_employee)
+                                        <tr>
+                                            <td>{{$key+1}}</td>
+                                            <td>{{isset($assigned_employee->employee_id) ? $assigned_employee->employee->firstname : ''}}</td>
+                                            <td class="text-nowrap">
+                                                <a class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirm-delete{{ $assigned_employee->id }}" title="Delete Employee from Skill"> <i class="fas fa-trash-alt text-white"></i></a>
+                                            </td>
+                                        </tr>
                                         <div class="modal fade" id="confirm-delete{{ $assigned_employee->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                                             <div class="modal-dialog">
                                                 <div class="modal-content">
                                                     <form action="{{ route('skill.unassign' , $assigned_employee->id )}}" method="post">
                                                         {{ csrf_field() }}
                                                         <div class="modal-header">
-                                                            Are you sure you want to Un-Assigned this Skill <br>Form Employee ?
+                                                            <h4 class="modal-title">Delete Employee from Skill</h4>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">×</span>
+                                                            </button>
                                                         </div>
-                                                        <div class="modal-header">
-                                                            <h4>{{isset($team_member->id) ? $team_member->employee->firstname . $team_member->employee->lastname: ''}}</h4>
+                                                        <div class="modal-body">
+                                                            Are you sure you want to Un-Assigned this Skill From Employee "{{$assigned_employee->employee->firstname}} {{$assigned_employee->employee->lastname}}"?
                                                         </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                                                            <button  type="submit" class="btn btn-danger btn-ok">Delete</button>
+                                                        <div class="modal-footer justify-content-between">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal" title="Cancel"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-window-close"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Cancel</span></button>
+                                                            <button  type="submit" class="btn btn-danger btn-ok" title="Delete Skill"><span class="d-xs-inline d-sm-none d-md-none d-lg-none"><i class="fas fa-trash-alt"></i></span><span class="d-none d-xs-none d-sm-inline d-md-inline d-lg-inline"> Delete</span></button>
                                                         </div>
                                                     </form>
                                                 </div>
                                             </div>
                                         </div>
-                                    </td>
-                                </tr>
-                            @endforeach @else
-                                <tr> No Employee Assigned This Skill</tr>
-                            @endif
-                            </tbody>
-                        </table>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<!-- Main Content End -->
+
+<script>
+    $(document).ready(function () {
+        $('#skilled_employees').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
 @stop
